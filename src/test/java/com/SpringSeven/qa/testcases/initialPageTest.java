82e6d85054f57d589c319f898afafafb7c2a8310
jarcibal <jericho.arcibal@swapoolabs.com>
2019-03-15 21:11:17 +0800

    9pm

--- src/test/java/com/SpringSeven/qa/testcases/initialPageTest.java
+++ src/test/java/com/SpringSeven/qa/testcases/initialPageTest.java
@@ -36,7 +36,7 @@ public class initialPageTest extends testBase{
 	
 	@Test
 	public void tester(){
-		driver.get("http://127.0.0.1:80");
+		driver.get("http://172.17.0.1/");
 		String value = driver.findElement(By.tagName("h1")).getText();
 		System.out.println(value);
 		}
