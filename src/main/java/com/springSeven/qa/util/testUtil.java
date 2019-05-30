11a216fa9cba2d84a0122db71a8c85fd2af6b1bc
jarcibal <jericho.arcibal@swapoolabs.com>
2019-05-30 13:10:41 +0800

    node max sessions

--- src/main/java/com/springSeven/qa/util/testUtil.java
+++ src/main/java/com/springSeven/qa/util/testUtil.java
@@ -91,7 +91,7 @@ public class testUtil extends testBase{
 	}
 	
 	//screenshot
-	public static void takeScreenshotAtEndOfTest() throws IOException {
+	public void takeScreenshotAtEndOfTest() throws IOException {
 		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 		String currentDir = System.getProperty("user.dir");
 		FileUtils.copyFile(scrFile, new File(currentDir + "/screenshots/" + System.currentTimeMillis() + ".png"));
