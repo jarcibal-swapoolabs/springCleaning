9e8f33d16e188710eca9826a691ae36e754385a5
jarcibal <jericho.arcibal@swapoolabs.com>
2019-06-12 22:50:15 +0800

    hoy

--- src/main/java/com/springSeven/qa/pages/productsPage.java
+++ src/main/java/com/springSeven/qa/pages/productsPage.java
@@ -33,8 +33,8 @@ public class productsPage extends testBase{
 	@FindBy(xpath="//span[contains(text(),'CANCEL')]")
 	public WebElement productsCancelButton;
 
-	//@FindBy(xpath="//span[contains(text(),'CONFIRM')]")
-	@FindBy(xpath="//button[@type='submit']")
+	@FindBy(xpath="//span[contains(text(),'CONFIRM')]")
+	//@FindBy(xpath="//button[@type='submit']")
 	public WebElement productsConfirmPurchaseButton;
 
 	//error
