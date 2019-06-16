4ae02043c0a26f6b0526f17ea49b110cb555a9cf
jarcibal <jericho.arcibal@swapoolabs.com>
2019-06-16 16:58:02 +0800

    hy

--- src/main/java/com/springSeven/qa/util/testUtil.java
+++ src/main/java/com/springSeven/qa/util/testUtil.java
@@ -141,7 +141,7 @@ public class testUtil extends testBase {
 	public void sendKeysUpload(WebElement element) {
 		{
 			String currentDir = System.getProperty("user.dir");
-			String toFile = (currentDir + "/screenshot.png");
+			String toFile = (currentDir + '/' + "screenshot.png");
 			if (driver instanceof RemoteWebDriver) {
 				((RemoteWebDriver) driver).setFileDetector(new LocalFileDetector());
 			}
